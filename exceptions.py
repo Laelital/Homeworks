75081fa54616ca15bf4d59af00c6df39ea574046
Laelital <56368810+Laelital@users.noreply.github.com>
2025-03-16 00:06:58 +0300

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,19 +1,37 @@
 class LowFuelError(Exception):
     """
-    Исключение при топливе меньше 0
+    Исключение при топливе = 0
     """
-    pass
+    def __init__(self, value, message='Количество топлива'):
+        self.value = value
+        self.message = message
+        super().__init__(self, message)
+
+    def __str__(self):
+        return f'{self.message}: {self.value}'
 
 
 class NotEnoughFuel(Exception):
     """
     Исключение при недостаточном количестве топлива
     """
-    pass
+    def __init__(self, value, message='Недостаточно топлива для расстояния'):
+        self.value = value
+        self.message = message
+        super().__init__(self, message)
+
+    def __str__(self):
+        return f'{self.message} = {self.value}'
 
 
 class CargoOverload(Exception):
     """
     Исключение при перегрузе
     """
-    pass
+    def __init__(self, value, message='Перегруз при добавлении'):
+        self.value = value
+        self.message = message
+        super().__init__(self, message)
+
+    def __str__(self):
+        return f'{self.message}: {self.value}'
