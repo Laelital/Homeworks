6548dc3c20c62a40eeb069976b1a80163b612f2c
Laelital <56368810+Laelital@users.noreply.github.com>
2025-03-31 19:23:40 +0300

    Update request_openbrewery.py

--- hw_4/api_files/openbrewery_api/request_openbrewery.py
+++ hw_4/api_files/openbrewery_api/request_openbrewery.py
@@ -1,24 +1,8 @@
 import requests
 
 
-def get_single_brewery(base_url, obdb_id):
-    res = requests.get(f'{base_url}/v1/breweries/{obdb_id}')
-    try:
-        return res
-    except requests.exceptions.RequestException:
-        print('Ошибка, статус код: ', {res.status_code})
-
-
-def get_list_breweries(base_url):
-    res = requests.get(f'{base_url}/v1/breweries')
-    try:
-        return res
-    except requests.exceptions.RequestException:
-        print('Ошибка, статус код: ', {res.status_code})
-
-
-def get_breweries_with_filter(base_url, flt, value):
-    res = requests.get(f'{base_url}/v1/breweries?by_{flt}={value}')
+def get_list_breweries_with_filter(base_url, flt='', num=1):
+    res = requests.get(f'{base_url}/v1/breweries?{flt}={num}')
     try:
         return res
     except requests.exceptions.RequestException:
@@ -27,14 +11,6 @@ def get_breweries_with_filter(base_url, flt, value):
 
 def get_random_brewery(base_url, value=1):
     res = requests.get(f'{base_url}/v1/breweries/random?size={value}')
-    try:
-        return res
-    except requests.exceptions.RequestException:
-        print('Ошибка, статус код: ', {res.status_code})
-
-
-def get_meta_data(base_url):
-    res = requests.get(f'{base_url}/v1/breweries/meta')
     try:
         return res
     except requests.exceptions.RequestException:
