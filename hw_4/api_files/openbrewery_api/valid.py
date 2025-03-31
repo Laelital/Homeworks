2a7cd70a7b19ee79c7a3afd4263b16fc8538f7d1
Laelital <56368810+Laelital@users.noreply.github.com>
2025-03-31 19:24:22 +0300

    Update valid.py

--- hw_4/api_files/openbrewery_api/valid.py
+++ hw_4/api_files/openbrewery_api/valid.py
@@ -5,21 +5,15 @@ class Brewery(BaseModel):
     id: str
     name: str
     brewery_type: str
-    address_1: str | None = None
-    address_2: str | None = None
-    address_3: str | None = None
+    address_1: str | None
+    address_2: str | None
+    address_3: str | None
     city: str
     state_province: str
     postal_code: str
     country: str
-    longitude: str | None = None
-    latitude: str | None = None
-    phone: str | None = None
-    website_url: str | None = None
-    street: str | None = None
-
-
-class Metadata(BaseModel):
-    total: str
-    page: str
-    per_page: str
+    longitude: float | None
+    latitude: float | None
+    phone: str | None
+    website_url: str | None
+    street: str | None
